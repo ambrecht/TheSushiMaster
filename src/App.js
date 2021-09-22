7f2cb5b9d4f25d3dcf3a3b17d431180002bae634
ambrecht <tino@ambrecht.de>
2021-09-22 12:10:55 +0200

    Anpassungen, Verbesserungen

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ import { updateGroup } from './utils/updateGroup';
 import { maxSeatCalculator } from './utils/maxSeatCalculator';
 import './styles.css';
 
+//The whole application runs in 4 phases:
+
+//1st phase Determine table size and create table.
+//2nd phase Create groups and place the groups.
+//3rd phase Remove groups from the table.
+//4th phase Place new groups and Update the table
+
 export default function App() {
   //The entire state of the application is located here
   const [Table, setTable] = useState([]);
@@ -18,9 +25,8 @@ export default function App() {
   const [MaxSeatRow, setMaxSeatRow] = useState(0);
 
   useEffect(() => {
-    console.log(UpdateStatus);
     setMaxSeatRow(maxSeatCalculator(Groups));
-  }, [Groups, UpdateStatus]);
+  }, [Groups]);
 
   //Determines the table size
   const saveTable = (MaxLength) => {
@@ -41,6 +47,14 @@ export default function App() {
     }
   };
 
+  const removeGroup = (GroupID) => {
+    const remove = Groups.map((e) => (e === GroupID ? (e = 'empty') : e));
+    const groupLength = Groups.filter((g) => g === GroupID);
+    setGroups(remove);
+    setUpdateStatus(true);
+    setSeatCounter(SeatCounter - groupLength.length);
+  };
+
   //Updates the groups when the table is full and the first groups leave it.
   const updateGroups = (GroupLength) => {
     setSeatCounter(SeatCounter + GroupLength);
@@ -48,18 +62,8 @@ export default function App() {
       console.log('Der Meister hat keinen Platz...');
     else {
       const update = updateGroup(Groups, GroupLength, Index);
-
-      console.log('Update', update);
       setGroups(update);
     }
-  };
-  const removeGroup = (GroupID) => {
-    const remove = Groups.map((e) => (e === GroupID ? (e = 'empty') : e));
-    const groupLength = Groups.filter((g) => g === GroupID);
-    console.log(groupLength);
-    setGroups(remove);
-    setUpdateStatus(true);
-    setSeatCounter(SeatCounter - groupLength.length);
   };
 
   return (
--- src/components/UpdateForm.js
+++ src/components/UpdateForm.js
@@ -14,7 +14,6 @@ export default function Groupform({
   const messager = () => (counter < tablelenght ? placeholder2 : placeholder3);
 
   const clickHandler = () => {
-    console.log(tablelenght - counter, Input);
     tablelenght - counter >= Input ? setInput(Input) : setInput('');
     onUpdateGroup(Number(Input));
   };
